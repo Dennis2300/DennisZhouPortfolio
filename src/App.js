13d17b15df8b26a3da41c1e6fb724e2c3153e234
Densanity <denniszixiang@gmail.com>
2024-10-09 18:56:23 +0200

    Tailwind and DaisyUI setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import FileUpload from "./components/FileUpload";
+//import FileUpload from "./components/FileUpload";
+//import ImageDisplay from "./components/ImageDisplay";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Firebase Image Upload</h1>
-        <FileUpload />
+        <h1 className="text-3xl font-bold underline">React Firebase Image Upload</h1>
       </header>
     </div>
   );
