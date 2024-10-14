10f7a1e280dc30d59ffe5870833b6ba3c0291829
Densanity <denniszixiang@gmail.com>
2024-10-14 16:41:37 +0200

    added home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      {/* Hero section on home page */}
+      <div id="homepage-hero-container">
+        <div
+          className="hero min-h-screen"
+          style={{
+            backgroundImage:
+              "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-neutral-content text-center">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Navbar on home page */}
+      <div className="navbar bg-base-100">
+        <div className="navbar-start"></div>
+
+        <div className="navbar-center hidden lg:flex">
+            {/*}
+        </div>
+        <div className="navbar-end">
+          {/* TODO: login button with google account */}
+        </div>
+      </div>
+    </div>
+  );
+}
