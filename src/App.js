bf0c15bb2d31d386cc956b95357728a1b2089f85
Densanity <denniszixiang@gmail.com>
2024-10-14 16:41:23 +0200

    added react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import "./App.css";
-//import FileUpload from "./components/FileUpload";
-//import ImageDisplay from "./components/ImageDisplay";
+
+// Pages import
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">React Firebase Image Upload</h1>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
