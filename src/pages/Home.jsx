d3cf30d7d5bc746dd39f35bb7b8fc8ccededbee8
Densanity <denniszixiang@gmail.com>
2024-10-18 19:57:00 +0200

    videoBg now works

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,56 @@ import React from "react";
 // import Components
 import Navbar from "../components/Navbar";
 
+// import Assets
+import videoBg from "../assets/videos/videoBg.mp4";
+
 export default function Home() {
   return (
     <div>
       {/* Hero section on home page */}
-      <div>
-        <h1>This is the Home Page</h1>
-        
+      <div id="homepage-hero-container">
+        <div className="hero min-h-screen">
+          <video
+            autoPlay
+            loop
+            muted
+            className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+          >
+            <source src={videoBg} type="video/mp4" />
+          </video>
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-neutral-content text-center">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
       </div>
 
       {/* Navbar on home page */}
       <Navbar />
 
       {/* Main content on home page */}
+
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
