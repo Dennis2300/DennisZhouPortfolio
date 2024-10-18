bfe0ef1be8a3476dcbc91fbf2f65d112f71ad9be
Densanity <denniszixiang@gmail.com>
2024-10-18 20:12:28 +0200

    made the navbar sticky

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,34 +3,32 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <>
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          {/* TODO: Whatever here logo or smth. */}
-        </div>
+    <div className="navbar bg-base-100 sticky top-0 z-50 shadow-lg">
+      <div className="navbar-start">
+        {/* TODO: Whatever here logo or smth. */}
+      </div>
 
-        <div className="navbar-center hidden lg:flex space-x-3">
-          {/* TODO: Button to link to pages */}
-          <div id="link-to-gallery-container">
-            <Link to="gallery">
-              <button>Gallery</button>
-            </Link>
-          </div>
-          <div id="link-to-about-container">
-            <Link to="about">
-              <button>About me</button>
-            </Link>
-          </div>
-          <div id="link-to-contact-container">
-            <Link to="contact">
-              <button>Contact</button>
-            </Link>
-          </div>
+      <div className="navbar-center hidden lg:flex space-x-3">
+        {/* TODO: Button to link to pages */}
+        <div id="link-to-gallery-container">
+          <Link to="gallery">
+            <button>Gallery</button>
+          </Link>
         </div>
-        <div className="navbar-end">
-          {/* TODO: login button with google account */}
+        <div id="link-to-about-container">
+          <Link to="about">
+            <button>About me</button>
+          </Link>
+        </div>
+        <div id="link-to-contact-container">
+          <Link to="contact">
+            <button>Contact</button>
+          </Link>
         </div>
       </div>
-    </>
+      <div className="navbar-end">
+        {/* TODO: login button with google account */}
+      </div>
+    </div>
   );
 }
