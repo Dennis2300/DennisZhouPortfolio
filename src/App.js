19669abfb2ec70e71337d1170b3401d87558b2d6
Densanity <denniszixiang@gmail.com>
2024-10-21 17:38:55 +0200

    added new routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import "./App.css";
 // Pages import
 import Home from './pages/Home';
 import Gallery from './pages/Gallery';
+import Admin from './pages/Admin';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
