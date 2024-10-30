98398db8c02ae15f93a995fa8f34eeb6ba073be8
Densanity <denniszixiang@gmail.com>
2024-10-30 17:06:35 +0100

    added dev notice to unfinished pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className="text-center">
+      <h1 className="text-7xl">Page is under developement</h1>
+      <p className="text-2xl mt-3">
+        This is my freetime project so I will be working on this in my sparetime
+      </p>
+      <div id="home-button-container">
+        <a
+          href="/"
+          className="mt-6 inline-block text-lg font-bold py-3 px-8 bg-white text-black rounded hover:bg-black hover:text-white"
+        >
+          Go to Homepage
+        </a>
+      </div>
     </div>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 export default function Contact() {
-    return (
-        <div>
-            <h1>Contact</h1>
-        </div>
-    );
+  return (
+    <div className="text-center">
+      <h1 className="text-7xl">Page is under developement</h1>
+      <p className="text-2xl mt-3">
+        This is my freetime project so I will be working on this in my sparetime
+      </p>
+      <div id="home-button-container">
+        <a
+          href="/"
+          className="mt-6 inline-block text-lg font-bold py-3 px-8 bg-white text-black rounded hover:bg-black hover:text-white"
+        >
+          Go to Homepage
+        </a>
+      </div>
+    </div>
+  );
 }
